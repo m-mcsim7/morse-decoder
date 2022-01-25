d8b2f44398346201ba39e188a3b24f89301c68d5
m-mcsim7 <m-mcsim7@yandex.by>
2022-01-25 10:09:50 +0300

    complite task

--- src/index.js
+++ src/index.js
@@ -1,46 +1,71 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  ".-": "a",
+  "-...": "b",
+  "-.-.": "c",
+  "-..": "d",
+  ".": "e",
+  "..-.": "f",
+  "--.": "g",
+  "....": "h",
+  "..": "i",
+  ".---": "j",
+  "-.-": "k",
+  ".-..": "l",
+  "--": "m",
+  "-.": "n",
+  "---": "o",
+  ".--.": "p",
+  "--.-": "q",
+  ".-.": "r",
+  "...": "s",
+  "-": "t",
+  "..-": "u",
+  "...-": "v",
+  ".--": "w",
+  "-..-": "x",
+  "-.--": "y",
+  "--..": "z",
+  ".----": "1",
+  "..---": "2",
+  "...--": "3",
+  "....-": "4",
+  ".....": "5",
+  "-....": "6",
+  "--...": "7",
+  "---..": "8",
+  "----.": "9",
+  "-----": "0",
+  "**********": " ",
 };
 
 function decode(expr) {
-    // write your solution here
-}
+   let strWithOutZero;
+   let strItog = "";
+   let arr = expr.match(/.{1,10}/g);
+   for (let i of arr) {
+     let arr10 = i.split("");
+ 
+     strWithOutZero = arr10.join("") 
+ 
+     for (let i = -1; i < arr10.length; ++i) {
+        if (arr10[i] == 0) {
+         delete arr10[i];
+         strWithOutZero = arr10.join("");
+       } 
+       else if (arr10[i] == 1) break
+ 
+       
+       //console.log(strWithOutZero)
+     }
+    // console.log(strWithOutZero)
+     let index = strWithOutZero.replace(/11/gi, "-").replace(/10/gi, ".");
+ 
+    // console.log(MORSE_TABLE[index] + ' = ' + index)
+     strItog = strItog + MORSE_TABLE[index];
+   }
+   return strItog;
+ }
 
 module.exports = {
-    decode
-}
+  decode,
+};
